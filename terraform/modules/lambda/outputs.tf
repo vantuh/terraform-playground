6091637bf9413f4b15c60286893073f16018d94c
Ivan Mykytiuk <i.mykytiuk@temabit.com>
2025-02-19 11:17:04 +0200

    feat: add additional lambda module outputs for invoke ARN and role name

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -7,3 +7,13 @@ output "function_name" {
   description = "The name of the lambda function"
   value       = aws_lambda_function.lambda.function_name
 }
+
+output "lambda_function_invoke_arn" {
+  description = "The invoke ARN of the lambda function"
+  value       = aws_lambda_function.lambda.invoke_arn
+}
+
+output "lambda_role_name" {
+  value       = aws_iam_role.lambda_role.name
+  description = "Name of the Lambda IAM role"
+}
