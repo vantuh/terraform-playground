953d01d1f84cba704dd082ae00a90b40f19eb545
Ivan Mykytiuk <i.mykytiuk@temabit.com>
2025-02-19 11:18:47 +0200

    feat: add apigw module to dev environment with outputs

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -8,3 +8,14 @@ module "lambda" {
   timeout               = var.lambda_timeout
   log_retention_in_days = var.log_retention_days
 }
+
+module "api" {
+  source = "../../modules/api"
+
+  api_name                   = "${var.project_name}-api-${var.environment}"
+  api_description            = "API for ${var.project_name} in ${var.environment}"
+  lambda_function_invoke_arn = module.lambda.lambda_function_invoke_arn
+  lambda_function_name       = module.lambda.function_name
+  lambda_role_name           = module.lambda.lambda_role_name
+  environment                = "prod"
+}
--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -5,3 +5,7 @@ output "lambda_function_name" {
 output "lambda_function_arn" {
   value = module.lambda.function_arn
 }
+
+output "api_url" {
+  value = module.api.api_url
+}
